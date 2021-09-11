10bc2b31fe704bcaa54b3361b557787a686f0d6f
Fabian Solheim <fabian.j.solheim@gmail.com>
2021-09-11 16:11:57 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,136 @@
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+import ReactMapGL, {
+  Marker,
+  FlyToInterpolator,
+  NavigationControl,
+} from "react-map-gl";
+import useSupercluster from "use-supercluster";
+
+
+const { REACT_APP_MAPBOX_TOKEN } = process.env;
+
+function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 59.913727,
+    longitude: 10.731024,
+    width: "100vw",
+    height: "100vh",
+    zoom: 11.4,
+  });
+  const mapRef = useRef();
+
+  const navControlStyle = {
+    right: 10,
+    top: 10,
+  };
+
+  const [voiScooters, setVoiScooters] = useState([]);
+  const voiUrl =
+    "https://api.entur.io/mobility/v2/gbfs/voioslo/free_bike_status";
+
+  const points = voiScooters.map((voi) => ({
+    type: "Feature",
+    properties: {
+      cluster: false,
+      voiId: voi.id,
+      category: "category",
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [voi.lon, voi.lat],
+    },
+  }));
+
+  const bounds = mapRef.current
+    ? mapRef.current.getMap().getBounds().toArray().flat()
+    : null;
+
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    zoom: viewport.zoom,
+    bounds,
+    options: { radius: 75, maxZoom: 20 },
+  });
+
+  async function fetchVoiScooters() {
+    const response = await fetch(voiUrl);
+    const voiData = await response.json();
+    setVoiScooters(voiData.data.bikes);
+  }
+
+  useEffect(() => {
+    fetchVoiScooters();
+  }, []);
+
+  return (
+    <div className="App">
+      <ReactMapGL
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+        maxZoom={20}
+        mapboxApiAccessToken={REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={(newViewport) => {
+          setViewport({ ...newViewport });
+        }}
+        ref={mapRef}
+      >
+        <NavigationControl style={navControlStyle} />
+        {clusters.map((cluster) => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const { cluster: isCluster, point_count: pointCount } =
+            cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker
+                key={cluster.id}
+                latitude={latitude}
+                longitude={longitude}
+              >
+                <div
+                  className="cluster-marker"
+                  style={{
+                    width: `${10 + (pointCount / points.length) * 30}px`,
+                    height: `${10 + (pointCount / points.length) * 30}px`,
+                  }}
+                  onClick={() => {
+                    const expansionZoom = Math.min(
+                      supercluster.getClusterExpansionZoom(cluster.id),
+                      20
+                    );
+                    setViewport({
+                      ...viewport,
+                      latitude,
+                      longitude,
+                      zoom: expansionZoom,
+                      transitionInterpolator: new FlyToInterpolator({
+                        speed: 2,
+                      }),
+                      transitionDuration: "auto",
+                    });
+                  }}
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+            );
+          }
+          return (
+            <Marker
+              key={cluster.properties.voiId}
+              latitude={latitude}
+              longitude={longitude}
+            >
+              <button className="scooter-marker">
+                <img src="./voi.png" alt="" />
+              </button>
+            </Marker>
+          );
+        })}
+      </ReactMapGL>
+    </div>
+  );
+}
+
+export default App;
